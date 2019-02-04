a81f61e38810abd405b6c3213d8a81923d62e9d4
shashwatacharya30 <kingsha.shashwat@gmail.com>
2019-02-04 14:39:05 +0000

    abc

--- amazon_books_scraping.py
+++ amazon_books_scraping.py
@@ -7,6 +7,10 @@ import random
 import csv
 import os
 
+
+
+
+
 options = Options()
 options.headless = True 
 timestamp = time.time()
@@ -22,11 +26,97 @@ csv_writer.writerow(['BOOK_Author', 'BOOK_NAME', 'Publisher', 'Language', 'Other
 
 driver = webdriver.Firefox(options = options)
 
-driver.get("https://www.amazon.com/best-sellers-books-Amazon/zgbs/books")
+
+
+options = Options()
+options.headless = True 
+timestamp = time.time()
 
 
 
+path = r'./data'
+if not os.path.exists(path):
+    os.makedirs(path)
+
+#csv_file = open('./data/'+str(timestamp)+'scraping_books.csv', 'w')
+
+def book_name():
+    try:
+        book_name = driver.find_element(By.CSS_SELECTOR,'li.zg-item-immersion:nth-child('+str(x)+') > span:nth-child(1) > div:nth-child(1) > span:nth-child(2) > a:nth-child(1) > div:nth-child(2)').text
+        print(book_name)
+        time.sleep(random.randint(1,8))
+    except Exception as e:
+        print(e)
+        
+    return(book_name)
+    
+def book_author():
+    try:
+        book_author = driver.find_element(By.CSS_SELECTOR,'li.zg-item-immersion:nth-child('+str(x)+') > span:nth-child(1) > div:nth-child(1) > span:nth-child(2) > div:nth-child(2) > a:nth-child(1)').text
+        print(book_author)
+        
+    except Exception as e:
+        print(e)
+        
+    return(book_author)
+    
+def book_link():
+    try:
+        book_link = driver.find_element(By.CSS_SELECTOR, 'li.zg-item-immersion:nth-child('+str(x)+') > span:nth-child(1) > div:nth-child(1) > span:nth-child(2) > a:nth-child(1)').get_attribute("href")
+        driver.get(book_link)
+    
+    except Exception as e:
+        print(e)
+        
+    return()
+
+def pd():
+    try:
+        pd=driver.find_element(By.CSS_SELECTOR, '.content > ul:nth-child(1) > li:nth-child(1)').text
+        print(pd)
+    except Exception as e:
+        print(e)
+        
+    return(pd)
+    
+def lan():
+    try:
+        lan = driver.find_element(By.CSS_SELECTOR, 'div.content:nth-child(2) > ul:nth-child(1) > li:nth-child(3)').text
+        print(lan)
+        
+    except Exception as e:
+        print(e)
+    
+    return(lan)
+
+def pub():
+    try:
+        pub = driver.find_element(By.CSS_SELECTOR, '.content > ul:nth-child(1) > li:nth-child(2)').text
+        print(pub)
+    except Exception as e:
+        print(e)
+        
+    return(pub)
+    
+def review():
+    try:
+        review = driver.find_elements_by_xpath('/html/body/div[2]/div[1]/div[4]/div[30]/div/div[2]/div/div[2]/span[3]/div/div/div[3]/div[3]/div/div[1]/div/div/div[4]/span/div/div[1]')
+        
+        for reviews in review:
+            
+            print(reviews.text)
+        
+    except Exception as e:
+        print(e)
+
+        
+    return()
+
+"""
+for x in range(1,50):
+=======
 for x in range(1,45):
+>>>>>>> fb8f753528e6deb58d3f9273e85cf7da5d1a28c3
     try:
         book_name = driver.find_element(By.CSS_SELECTOR,'li.zg-item-immersion:nth-child('+str(x)+') > span:nth-child(1) > div:nth-child(1) > span:nth-child(2) > a:nth-child(1) > div:nth-child(2)').text
         print(book_name)
@@ -68,9 +158,43 @@ for x in range(1,45):
     except Exception as e :
         print(e)
 
+"""
+if __name__ == "__main__":
+    
+    csv_file = open('./data/'+str(timestamp)+'scraping_books.csv', 'w')
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['BOOK_Author', 'BOOK_NAME', 'Publisher', 'Language', 'Other Details'])
+
+    driver = webdriver.Firefox(options = options)
+
+
+    driver.get("https://www.amazon.com/best-sellers-books-Amazon/zgbs/books")
+
+    for x in range(1,20):
+        try:
+            
+            name=book_name()
+            author = book_author()
+            book_link()
+            pages = pd()
+            languages = lan()
+            publication = pub()
+            review()
+            driver.get("https://www.amazon.com/best-sellers-books-Amazon/zgbs/books")
+            csv_writer.writerow([author,name,publication,languages,pages])
+            
+        except Exception as e:
+            print(e)
+
+  
+  
+  
+  
+  
+  
+  
 
 csv_file.close()
-
 driver.quit()
 
 
